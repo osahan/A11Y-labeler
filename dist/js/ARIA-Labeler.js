266713bd6811aa92c2e3cb0469a9f83c1d72956e
osahan <robi_osahan@yahoo.com>
2015-01-03 17:41:02 -0800

    grunt dist

---
+++ dist/js/ARIA-Labeler.js
@@ -0,0 +1,90 @@
+/**
+ * @fileOverview This is the top-level file for ARIA-labeller.
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+var aria = window.aria || {};
+
+aria.version = '1.0.0';
+aria.options = {
+    role: true,
+    label: true
+};
+/**
+ * @fileOverview anchor.js traverse throught DOM and assign ARIA labels to all anchors.
+ *
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+(function(window, $, undefined){
+
+    aria.anchor = function(options){
+
+
+        var ele = $("a"),
+            regx = /(^#|#$)/,
+            labeller = {
+
+                init: function(){
+
+                    this.setOptions();
+
+                    if(options.role){
+
+                        this.defineRole();
+
+                    }
+
+                    if(options.label){
+
+                        this.assignLabels();
+
+                    }
+
+                },
+
+                setOptions: function(){
+
+                    options.role = options.role || aria.options.role;
+                    options.label = options.label || aria.options.label;
+
+                },
+
+                defineRole: function(){
+
+                    $.each(ele, function( index, value ) {
+
+                        if( regx.test($(value).prop("href")) && typeof $(value).prop("role") === "undefined"){
+
+                            $(value).attr("role", "button");
+
+                        }
+                        else if( !regx.test($(value).prop("href")) && typeof $(value).prop("role") === "undefined"){
+
+                            $(value).attr("role", "link");
+
+                        }
+
+                    });
+
+                },
+
+                assignLabels: function(){
+
+
+                },
+
+                uiStates: function(){
+
+                }
+            };
+
+        labeller.init();
+
+    };
+
+})(this, jQuery);
+
+aria.anchor();
