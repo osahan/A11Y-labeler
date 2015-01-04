d09baf0f34d3732e7a4248ff8e58ad1376163d3c
osahan <robi_osahan@yahoo.com>
2015-01-03 17:40:15 -0800

    Init file

---
+++ js/aria.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview This is the top-level file for ARIA-labeller.
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+var aria = window.aria || {};
+
+aria.version = '1.0.0';
+aria.options = {
+    role: true,
+    label: true
+};
