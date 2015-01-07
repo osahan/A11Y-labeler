0210067e9def2e1d7acc8a6483298b3ed88b48d1
osahan <robi_osahan@yahoo.com>
2015-01-06 20:39:50 -0800

    Adding config file

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/**
+ * @fileOverview This is the Config file for ARIA-labeller, where we call all the plugins.
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+var options = {
+    role: true,
+    label: true
+};
+
+$("a").anchor(options);
