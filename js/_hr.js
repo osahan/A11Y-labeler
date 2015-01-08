1df5bd37ea8ae9c8605ef954eaa439d933a54993
osahan <robi_osahan@yahoo.com>
2015-01-08 09:19:53 -0800

    Adding initial file

---
+++ js/_hr.js
@@ -0,0 +1,69 @@
+/**
+ * @fileOverview area.js traverse throught DOM and assign ARIA labels to all area elements with href.
+ *
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+(function(window, $, undefined){
+
+    aria._hr = function(options){
+
+
+        var ele = $("hr"),
+            labeller = {
+
+                init: function(){
+
+                    this.setOptions();
+
+                    if(options.role){
+
+                        this.defineRole();
+
+                    }
+
+                    if(options.label){
+
+                        this.assignLabels();
+
+                    }
+
+                },
+
+                setOptions: function(){
+
+                    options.role = options.role || aria.options.role;
+                    options.label = options.label || aria.options.label;
+
+                },
+
+                defineRole: function(){
+
+                    $.each(ele, function( index, value ) {
+
+                        if( typeof $(value).prop("role") === "undefined"){
+
+                            $(value).attr("role", "separator");
+
+                        }
+
+                    });
+
+                },
+
+                assignLabels: function(){
+
+
+                },
+
+                uiStates: function(){
+
+                }
+            };
+
+        labeller.init();
+
+    };
+
+})(this, jQuery);
