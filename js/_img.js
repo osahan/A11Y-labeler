00c9644548e16b6f5c756125afbd1dda7cf9db22
osahan <robi_osahan@yahoo.com>
2015-01-08 09:18:13 -0800

    Adding initial file

---
+++ js/_img.js
@@ -0,0 +1,75 @@
+/**
+ * @fileOverview area.js traverse throught DOM and assign ARIA labels to all area elements with href.
+ *
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+(function(window, $, undefined){
+
+    aria._img = function(options){
+
+
+        var ele = $("img"),
+            labeller = {
+
+                init: function(){
+
+                    this.setOptions();
+
+                    if(options.role){
+
+                        this.defineRole();
+
+                    }
+
+                    if(options.label){
+
+                        this.assignLabels();
+
+                    }
+
+                },
+
+                setOptions: function(){
+
+                    options.role = options.role || aria.options.role;
+                    options.label = options.label || aria.options.label;
+
+                },
+
+                defineRole: function(){
+
+                    $.each(ele, function( index, value ) {
+
+                        if( $(value).prop("alt") && typeof $(value).prop("role") === "undefined"){
+
+                            $(value).attr("role", "img");
+
+                        }
+                        else if( !$(value).prop("alt") && typeof $(value).prop("role") === "undefined"){
+
+                            $(value).attr("role", "presentation");
+
+                        }
+
+
+                    });
+
+                },
+
+                assignLabels: function(){
+
+
+                },
+
+                uiStates: function(){
+
+                }
+            };
+
+        labeller.init();
+
+    };
+
+})(this, jQuery);
