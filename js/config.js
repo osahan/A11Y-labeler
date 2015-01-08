9db03cf1ef9651f5116bc78fdec7a36d360d5502
osahan <robi_osahan@yahoo.com>
2015-01-08 09:20:07 -0800

    adding new functions

--- js/config.js
+++ js/config.js
@@ -6,7 +6,25 @@
 
     var config  = function(options){
 
-        aria.anchor(options);
+        aria._anchor(options);
+        aria._area(options);
+        aria._article(options);
+        aria._aside(options);
+        aria._body(options);
+        aria._button(options);
+        aria._datalist(options);
+        aria._details(options);
+        aria._dl(options);
+        aria._form(options);
+        aria._h1(options);
+        aria._h2(options);
+        aria._h3(options);
+        aria._h4(options);
+        aria._h5(options);
+        aria._h6(options);
+        aria._hr(options);
+        aria._img(options);
+        aria._input(options);
 
     };
 
