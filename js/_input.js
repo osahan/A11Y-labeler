36279fda5b67f852d4e7dd34a84607d9fa8aaa45
osahan <robi_osahan@yahoo.com>
2015-01-08 09:18:07 -0800

    Adding initial file

---
+++ js/_input.js
@@ -0,0 +1,77 @@
+/**
+ * @fileOverview area.js traverse throught DOM and assign ARIA labels to all area elements with href.
+ *
+ * @author Gagandeep Singh <robi_osahan@yahoo.com>
+ * @version 1.0.0
+ */
+
+(function(window, $, undefined){
+
+    aria._input = function(options){
+
+
+        var ele = $("input"),
+            labeller = {
+
+                init: function(){
+
+                    this.setOptions();
+
+                    if(options.role){
+
+                        this.defineRole();
+
+                    }
+
+                    if(options.label){
+
+                        this.assignLabels();
+
+                    }
+
+                },
+
+                setOptions: function(){
+
+                    options.role = options.role || aria.options.role;
+                    options.label = options.label || aria.options.label;
+
+                },
+
+                defineRole: function(){
+
+                    $.each(ele, function( index, value ) {
+
+
+console.log( this );
+                        // if( $(value).prop("alt") && typeof $(value).prop("role") === "undefined"){
+
+                        //     $(value).attr("role", "img");
+
+                        // }
+                        // else if( !$(value).prop("alt") && typeof $(value).prop("role") === "undefined"){
+
+                        //     $(value).attr("role", "presentation");
+
+                        // }
+
+
+                    });
+
+                },
+
+                assignLabels: function(){
+
+
+                },
+
+                uiStates: function(){
+
+                }
+            };
+
+        labeller.init();
+
+    };
+
+})(this, jQuery);
