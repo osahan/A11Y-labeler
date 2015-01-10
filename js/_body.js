03d7307a54568be6b6f4cf1baa9d096a2ea933c0
Osahan <robi_osahan@yahoo.com>
2015-01-09 21:11:13 -0800

    Removing setOptions

--- js/_body.js
+++ js/_body.js
@@ -1,5 +1,5 @@
 /**
- * @fileOverview area.js traverse throught DOM and assign ARIA labels to all area elements with href.
+ * @fileOverview _body.js traverse throught DOM and assign ARIA labels to all area elements with href.
  *
  * @author Gagandeep Singh <robi_osahan@yahoo.com>
  * @version 1.0.0
@@ -7,34 +7,17 @@
 
 (function(window, $, undefined){
 
-    aria._body = function(options){
+    aria._body = function(){
 
 
         var ele = aria.currentSelector.find("body"),
-            labeller = {
+            self = this,
+            labeler = {
 
                 init: function(){
 
-                    this.setOptions();
-
-                    if(options.role){
-
-                        this.defineRole();
-
-                    }
-
-                    if(options.label){
-
-                        this.assignLabels();
-
-                    }
-
-                },
-
-                setOptions: function(){
-
-                    options.role = options.role || aria.options.role;
-                    options.label = options.label || aria.options.label;
+                    this.defineRole();
+                    this.assignLabels();
 
                 },
 
@@ -62,7 +45,7 @@
                 }
             };
 
-        labeller.init();
+        labeler.init();
 
     };
 
